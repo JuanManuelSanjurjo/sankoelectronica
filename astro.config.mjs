eb04a50e51e2157f0834399397ceafe2a8622215
JuanManuelSanjurjo <juanmanuelsanjurjo@gmail.com>
2024-11-26 15:03:25 -0300

    site config to netlify url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   base: "/",
+  site: "https://sanko-electronica.ntelift.app",
   integrations: [react()],
 });
